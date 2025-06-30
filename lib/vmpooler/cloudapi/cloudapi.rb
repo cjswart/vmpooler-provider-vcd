97f7d726b98713ba14f15fdb1cbc6b547adb42ad
cswart <cees-jan.swart@kpn.com>
2025-06-30 23:58:43 +0200

    fix

--- lib/vmpooler/cloudapi/cloudapi.rb
+++ lib/vmpooler/cloudapi/cloudapi.rb
@@ -298,26 +298,34 @@ class CloudAPI
       os_drive_storage_tier_href = cloudapi_get_storage_policy_href(pool, connection)
       catalogItem_href = cloudapi_get_catalog_item_href(pool, connection)
       # Prepare the XML body for the VM creation request
+      # IpAddressAllocationMode can be one of the following (as per VMware vCloud API):
+      # - "POOL": IP is allocated from the network's IP pool
+      # - "DHCP": IP is allocated via DHCP
+      # - "MANUAL": IP is manually specified
+      # - "NONE": No IP allocation
+      #
+      # Example usage:
+      # <root:IpAddressAllocationMode>DHCP</root:IpAddressAllocationMode>
       xml_body = <<~XML
         <root:RecomposeVAppParams xmlns:root="http://www.vmware.com/vcloud/v1.5" xmlns:ns0="http://schemas.dmtf.org/ovf/envelope/1">
           <root:SourcedItem>
-              <root:Source href="#{catalogItem_href}"
-              name="#{new_vmname}"
-              type="application/vnd.vmware.vcloud.vm+xml"/>
-              <root:InstantiationParams>
-                 <root:NetworkConnectionSection><ns0:Info/>
-                  <root:PrimaryNetworkConnectionIndex>0</root:PrimaryNetworkConnectionIndex>
-                  <root:NetworkConnection network="#{pool['network']}">
-                    <root:NetworkConnectionIndex>0</root:NetworkConnectionIndex>
-                    <root:IpAddress/>
-                    <root:IpType>IPV4</root:IpType>
-                    <root:IsConnected>true</root:IsConnected>
-                    <root:IpAddressAllocationMode>DHCP</root:IpAddressAllocationMode>
-                    <root:NetworkAdapterType>VMXNET3</root:NetworkAdapterType>
-                  </root:NetworkConnection>
-                </root:NetworkConnectionSection>
-              </root:InstantiationParams>
-            <root:StorageProfile href="#{os_drive_storage_tier_href}" type="application/vnd.vmware.vcloud.vdcStorageProfile+xml"/>
+          <root:Source href="#{catalogItem_href}"
+          name="#{new_vmname}"
+          type="application/vnd.vmware.vcloud.vm+xml"/>
+          <root:InstantiationParams>
+             <root:NetworkConnectionSection><ns0:Info/>
+          <root:PrimaryNetworkConnectionIndex>0</root:PrimaryNetworkConnectionIndex>
+          <root:NetworkConnection network="#{pool['network']}">
+            <root:NetworkConnectionIndex>0</root:NetworkConnectionIndex>
+            <root:IpAddress/>
+            <root:IpType>IPV4</root:IpType>
+            <root:IsConnected>true</root:IsConnected>
+            <root:IpAddressAllocationMode>POOL</root:IpAddressAllocationMode>
+            <root:NetworkAdapterType>VMXNET3</root:NetworkAdapterType>
+          </root:NetworkConnection>
+            </root:NetworkConnectionSection>
+          </root:InstantiationParams>
+        <root:StorageProfile href="#{os_drive_storage_tier_href}" type="application/vnd.vmware.vcloud.vdcStorageProfile+xml"/>
           </root:SourcedItem>
           <root:AllEULAsAccepted>true</root:AllEULAsAccepted>
         </root:RecomposeVAppParams>
